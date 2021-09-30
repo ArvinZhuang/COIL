c4f4830d1af9a42b13cdeb782aa9acc9c2cb9260
ArvinZhuang <shengyaozhuang@gmail.com>
2021-09-30 17:56:01 +1000

    update query expansion experiments code

---
+++ convert_to_pyserini_uniCOIL.py
@@ -0,0 +1,123 @@
+from typing import Optional
+from transformers import BertConfig, BertModel, BertTokenizer, PreTrainedModel
+import os
+import torch
+from argparse import ArgumentParser
+
+
+class UniCoilEncoder(PreTrainedModel):
+    config_class = BertConfig
+    base_model_prefix = 'coil_encoder'
+    load_tf_weights = None
+
+    def __init__(self, config: BertConfig):
+        super().__init__(config)
+        self.config = config
+        self.bert = BertModel(config)
+        self.tok_proj = torch.nn.Linear(config.hidden_size, 1)
+        self.init_weights()
+
+    # Copied from transformers.models.bert.modeling_bert.BertPreTrainedModel._init_weights
+    def _init_weights(self, module):
+        """ Initialize the weights """
+        if isinstance(module, (torch.nn.Linear, torch.nn.Embedding)):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+        elif isinstance(module, torch.nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, torch.nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+
+    def init_weights(self):
+        self.bert.init_weights()
+        self.tok_proj.apply(self._init_weights)
+
+    def forward(
+            self,
+            input_ids: torch.Tensor,
+            attention_mask: Optional[torch.Tensor] = None,
+    ):
+        input_shape = input_ids.size()
+        device = input_ids.device
+        if attention_mask is None:
+            attention_mask = (
+                torch.ones(input_shape, device=device)
+                if input_ids is None
+                else (input_ids != self.bert.config.pad_token_id)
+            )
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        sequence_output = outputs.last_hidden_state
+        tok_weights = self.tok_proj(sequence_output)
+        tok_weights = torch.relu(tok_weights)
+        return tok_weights
+
+
+
+class UniCoilEncoderToSave(PreTrainedModel):
+    config_class = BertConfig
+    base_model_prefix = 'bert'
+    load_tf_weights = None
+
+    def __init__(self, config: BertConfig):
+        super().__init__(config)
+        self.config = config
+        self.bert = BertModel(config)
+        self.tok_proj = torch.nn.Linear(config.hidden_size, 1)
+        self.init_weights()
+
+    # Copied from transformers.models.bert.modeling_bert.BertPreTrainedModel._init_weights
+    def _init_weights(self, module):
+        """ Initialize the weights """
+        if isinstance(module, (torch.nn.Linear, torch.nn.Embedding)):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+        elif isinstance(module, torch.nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, torch.nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+
+    def init_weights(self):
+        self.bert.init_weights()
+        model_dict = torch.load(os.path.join(self.config.name_or_path, 'model.pt'), map_location="cpu")
+        self.load_state_dict(model_dict, strict=False)
+
+
+    def forward(
+            self,
+            input_ids: torch.Tensor,
+            attention_mask: Optional[torch.Tensor] = None,
+    ):
+        input_shape = input_ids.size()
+        device = input_ids.device
+        if attention_mask is None:
+            attention_mask = (
+                torch.ones(input_shape, device=device)
+                if input_ids is None
+                else (input_ids != self.bert.config.pad_token_id)
+            )
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        sequence_output = outputs.last_hidden_state
+        tok_weights = self.tok_proj(sequence_output)
+        tok_weights = torch.relu(tok_weights)
+        return tok_weights
+
+
+def convert(args):
+    tokenizer = BertTokenizer.from_pretrained(args.ckpt_path)
+    model = UniCoilEncoderToSave.from_pretrained(args.ckpt_path)
+
+    model.save_pretrained(args.save_path)
+    tokenizer.save_pretrained(args.save_path)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('--ckpt_path', type=str, required=True)
+    parser.add_argument('--save_path', type=str, required=True)
+    args = parser.parse_args()
+
+    convert(args)
--- data_helpers/decode_index_files.py
+++ data_helpers/decode_index_files.py
@@ -1,21 +1,34 @@
 import os, glob
 import json
-from transformers import BertTokenizerFast
+from transformers import BertTokenizer
 from tqdm import tqdm
+from argparse import ArgumentParser
 
-file_path = '../msmarco_passage_unicoil_encoded_TILDE_200'
-output_path = '../msmarco_passage_unicoil_encoded_TILDE_200_decoded'
-tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
 
-files = glob.glob(os.path.join(file_path, '*'))
-for file in tqdm(files):
-    with open(file, 'r') as f, open(f"{output_path}/{file.split('/')[-1]}", 'w') as wf:
-        for line in f:
-            data = json.loads(line)
-            vector = {}
-            for tok_id in data['vector'].keys():
-                vector[tokenizer.decode(int(tok_id))] = data['vector'][tok_id]
-            data['vector'] = vector
-            json.dump(data, wf)
-            wf.write('\n')
-    # print(file.split('/')[-1])
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('--file_path', type=str, required=True)
+    parser.add_argument('--output_path', type=str, required=True)
+    args = parser.parse_args()
+
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path)
+
+    # file_path = '../msmarco_passage_unicoil_encoded_TILDE_200'
+    # output_path = '../msmarco_passage_unicoil_encoded_TILDE_200_decoded'
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', use_fast=True, cache_dir="../cache")
+
+    files = glob.glob(os.path.join(args.file_path, '*'))
+    for file in tqdm(files):
+        with open(file, 'r') as f, open(f"{args.output_path}/{file.split('/')[-1]}", 'w') as wf:
+            for line in f:
+                data = json.loads(line)
+                vector = {}
+                for tok_id in data['vector'].keys():
+                    vector[tokenizer.decode([int(tok_id)])] = data['vector'][tok_id]
+                data['vector'] = vector
+                json.dump(data, wf)
+                wf.write('\n')
+
